86a602b3b1c7658d077e02bd567ceb5d35fc50b7
Karl Edward Bayron <karl.bayron@paymaya.com>
2021-10-18 11:41:29 +0800

    Added session variables

--- index.js
+++ index.js
@@ -5,14 +5,27 @@ const {authRoutes, displayName} = require('./routes/auth.js');
 const mongoose = require('mongoose');
 const app = express();
 const PORT = 5000;
+const passport = require('passport');
+//const cookieSession = require('cookie-session');
+const session = require('express-session'); 
+
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: 'cadetship2021'}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.listen(PORT, () => console.log(`Server Running on port http://localhost:${PORT}`));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
 mongoose.connect('mongodb+srv://cadetship:Blue%40123%21@cluster0.rceks.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', () => console.log('connected to DB!'));
 
 app.get('/', (req,res) => {
-    res.send(`${displayName}`);
+    console.log(req.session);
+    res.send(`${req.session.passport.user.displayName}`);
 });
 
 app.use('/users', usersRoutes);
--- routes/auth.js
+++ routes/auth.js
@@ -1,20 +1,11 @@
 const express = require('express');
 const authRoutes = express.Router();
 const passport = require('passport');
-const cookieSession = require('cookie-session');
-
 var googleId = null;
 var displayName = null;
-
 require('../passport');
 
-authRoutes.use(cookieSession({
-    name: 'google-auth-session',
-    keys: ['key1', 'key2']
-  }));
 
-authRoutes.use(passport.initialize());
-authRoutes.use(passport.session());
 
 authRoutes.get('/',
     passport.authenticate('google', {
@@ -37,6 +28,7 @@ authRoutes.get("/failed", (req, res) => {
     res.send("Failed")
 });
 authRoutes.get("/success", (req, res) => {
+    //console.log(req.session)
     displayName = req.user.displayName
     googleId = req.user.id
     res.send(`Welcome ${displayName}`)
